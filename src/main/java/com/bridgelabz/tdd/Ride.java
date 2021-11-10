86bd4dbcb53f42788ea1e1114dcbdba16c1935f1
unknown <rajputnikita5666@gmail.com>
2021-11-10 23:12:31 +0530

    Added:Multiple ride program

---
+++ src/main/java/com/bridgelabz/tdd/Ride.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.tdd;
+
+public class Ride {
+	public final int time;
+	public double distance;
+
+	// parameterized Constructor
+	public Ride(double distance, int time) {
+		this.distance = distance;
+		this.time = time;
+	}
+}
