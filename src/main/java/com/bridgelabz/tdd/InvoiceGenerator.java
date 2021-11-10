d7f07d674690b65d0a2539473c33ab0a44856a61
unknown <rajputnikita5666@gmail.com>
2021-11-10 23:19:04 +0530

    Added:Enhanced invoice program

--- src/main/java/com/bridgelabz/tdd/InvoiceGenerator.java
+++ src/main/java/com/bridgelabz/tdd/InvoiceGenerator.java
@@ -13,12 +13,21 @@ public class InvoiceGenerator {
 			return totalFare;
 	}
 
-	   //calculating Fare for Multiple rides
-    public double calculateFare(Ride[] rides){
-        double totalFare = 0;
-        for (Ride ride : rides){
-            totalFare += this.calculateFare(ride.distance, ride.time);
-        }
-        return totalFare;
-    }
+	// calculating Fare for Multiple rides
+	public double calculateFare(Ride[] rides) {
+		double totalFare = 0;
+		for (Ride ride : rides) {
+			totalFare += this.calculateFare(ride.distance, ride.time);
+		}
+		return totalFare;
+	}
+
+	// calculating totalFare, average Fare for multiple rides
+	public InvoiceSummary getInvoiceSummary(Ride[] rides) {
+		double totalFare = 0;
+		for (Ride ride : rides) {
+			totalFare += this.calculateFare(ride.distance, ride.time);
+		}
+		return new InvoiceSummary(rides.length, totalFare);
+	}
 }
--- src/test/java/com/bridgelabz/tdd/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabz/tdd/InvoiceServiceTest.java
@@ -22,4 +22,16 @@ public class InvoiceServiceTest {
         double fare = invoiceGenerator.calculateFare(rides);
         Assert.assertEquals(30, fare, 0.0);
     }
+    
+
+    //testcase should return the Invoice Summary
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary() {
+        Ride[] rides = {new Ride(5.0, 10),
+                new Ride(10, 20)
+        };
+        InvoiceSummary invoiceSummary = InvoiceGenerator.getInvoiceSummary(rides);
+        InvoiceSummary summary = new InvoiceSummary(2, 180);
+        Assert.assertEquals(summary, invoiceSummary);
+    }
 }
